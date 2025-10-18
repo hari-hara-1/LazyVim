56069a4be68edaee5cd6f8accb0493e8a5767094
hari-hara-1 <hari.shenbagan@gmail.com>
2025-10-18 10:16:39 +0530

    Catppuccin Color Scheme. This commit of Lazyvim uses catppuccin color scheme.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
   -- Add the Gruvbox theme plugin
   { "ellisonleao/gruvbox.nvim" },
-{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false },
+{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "moonfly",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
