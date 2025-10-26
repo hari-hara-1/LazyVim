8576bb68a1a7785b1c539ff2003cfe8ae6643c4f
hari-hara-1 <hari.shenbagan@gmail.com>
2025-10-26 19:16:58 +0530

    Update README.md file, Added config to neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,24 +1,35 @@
+-- lazy.nvim
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      -- Whether to group “empty” directories (dirs with only one child) into a combined view
-      group_empty_dirs = true,
-
-      -- If set to "deep", scanning will look further to collapse deeper chains
-      scan_mode = "deep",
-
-      -- other usual options ...
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        -- etc.
-      },
-      follow_current_file = {
-        enabled = true,
-      },
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      -- Others dependencies
+      'saifulapm/neotree-file-nesting-config', -- add plugin as dependency. no need any other config or setup call
     },
-
-    -- (optional) you might need to adjust other window, mapping, etc.
-  },
+    opts = {
+      -- recommanded config for better UI
+      hide_root_node = true,
+      retain_hidden_root_indent = true,
+      filesystem = {
+        filtered_items = {
+          show_hidden_count = false,
+          never_show = {
+            '.DS_Store',
+          },
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true,
+          expander_collapsed = '',
+          expander_expanded = '',
+        },
+      },
+      -- others config
+    },
+    config = function(_, opts)
+      -- Adding rules from plugin
+      opts.nesting_rules = require('neotree-file-nesting-config').nesting_rules
+      require('neo-tree').setup(opts)
+    end,
 }
