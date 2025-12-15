6876c7d09c4c2200466282a2636794e7ba5963e0
hari-hara-1 <hari.shenbagan@gmail.com>
2025-12-15 20:47:03 +0530

    Added plugins for LaTeX

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,5 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 vim.g.autoformat = false
+vim.g.vimtex_view_method = "skim"
+
